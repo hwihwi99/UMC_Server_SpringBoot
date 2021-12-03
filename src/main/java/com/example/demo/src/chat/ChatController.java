764bcd3627a6908d1f21f6a092a26e817edcf77d
hwihwi99 <chamgrace@naver.com>
2021-12-03 21:15:08 +0900

    채팅부분 페이징 처리

--- src/main/java/com/example/demo/src/chat/ChatController.java
+++ src/main/java/com/example/demo/src/chat/ChatController.java
@@ -1,16 +1,21 @@
 package com.example.demo.src.chat;
 
 import com.example.demo.config.BaseException;
+import com.example.demo.config.BasePageResponse;
 import com.example.demo.config.BaseResponse;
 import com.example.demo.src.chat.model.GetChattingDetailReq;
 import com.example.demo.src.chat.model.GetChattingRes;
 import com.example.demo.src.chat.model.GetChattingRoomListRes;
+import com.example.demo.src.item.model.GetItemRes;
+import com.example.demo.src.paging.PageProvider;
+import com.example.demo.src.paging.model.GetPageInfo;
 import com.example.demo.utils.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.example.demo.config.BaseResponseStatus.INVALID_USER_JWT;
@@ -26,46 +31,79 @@ public class ChatController {
     private final ChatService chatService;
     @Autowired
     private final JwtService jwtService;
-
-    public ChatController(ChatProvider chatProvider, ChatService chatService, JwtService jwtService){
+    @Autowired
+    private final PageProvider pageProvider;
+    private int currentPosition,start,last;
+    public ChatController(ChatProvider chatProvider, ChatService chatService, JwtService jwtService, PageProvider pageProvider){
         this.chatProvider = chatProvider;
         this.chatService = chatService;
         this.jwtService = jwtService;
+        this.pageProvider = pageProvider;
     }
 
+    // 해당 사용자에 맞는 모든 채팅방 목록 보여주기
     @ResponseBody
     @GetMapping("/list/{nickname}")
-    public BaseResponse<List<GetChattingRoomListRes>> chattingList(@PathVariable("nickname") String nickname){
+    public BasePageResponse<GetPageInfo,List<GetChattingRoomListRes>> chattingList(@PathVariable("nickname") String nickname){
         try{
             String userNicknameByJwt = jwtService.getNickname();
-            System.out.println(userNicknameByJwt);
-            System.out.println(nickname);
             if(!nickname.equals(userNicknameByJwt)){
-                return new BaseResponse<>(INVALID_USER_JWT);
+                return new BasePageResponse<>(INVALID_USER_JWT);
             }
-            System.out.println("1");
             List<GetChattingRoomListRes> chattingList = chatProvider.getChattingRoomList(nickname);
-            return new BaseResponse<>(chattingList);
+            GetPageInfo getPageInfo = pageProvider.getPagingInfo(chattingList.size());
+            List<GetChattingRoomListRes> listItemRes = new ArrayList<>();
+
+            currentPosition = (int)(Math.ceil(getPageInfo.getCurrentPage() / getPageInfo.getCountList())) ;
+            if(getPageInfo.getCurrentPage() %3 != 0){
+                currentPosition++;
+            }
+            start = (currentPosition-1) * getPageInfo.getCountList() + 1;
+            last = (currentPosition) * getPageInfo.getCountList();
+
+
+            for(int i = 0;i<chattingList.size(); i++){
+                if(i>=start-1 && i<=last-1){
+                    listItemRes.add(chattingList.get(i));
+                }
+            }
+            return new BasePageResponse<>(getPageInfo,chattingList);
         } catch (BaseException exception){
-            return new BaseResponse<>((exception.getStatus()));
+            return new BasePageResponse<>((exception.getStatus()));
         }
     }
 
     @ResponseBody
     @GetMapping("/detail/{roomIdx}")
-    public BaseResponse<List<GetChattingRes>> chattingDetail(@PathVariable("roomIdx") int roomIdx,@RequestBody GetChattingDetailReq getChattingDetailReq){
+    public BasePageResponse<GetPageInfo,List<GetChattingRes>> chattingDetail(@PathVariable("roomIdx") int roomIdx,@RequestBody GetChattingDetailReq getChattingDetailReq){
         getChattingDetailReq.setRoomIdx(roomIdx);
         try{
             String userNicknameByJwt = jwtService.getNickname();
-            System.out.println(userNicknameByJwt+"1");
-            System.out.println(getChattingDetailReq.getNickname()+"2");
             if(!getChattingDetailReq.getNickname().equals(userNicknameByJwt)){
-                return new BaseResponse<>(INVALID_USER_JWT);
+                return new BasePageResponse<>(INVALID_USER_JWT);
             }
             List<GetChattingRes> chattingDetail = chatProvider.chattingList(getChattingDetailReq);
-            return new BaseResponse<>(chattingDetail);
+            GetPageInfo getPageInfo = pageProvider.getPagingInfo(chattingDetail.size());
+
+            List<GetChattingRes> listItemRes = new ArrayList<>();
+
+            currentPosition = (int)(Math.ceil(getPageInfo.getCurrentPage() / getPageInfo.getCountList())) ;
+            if(getPageInfo.getCurrentPage() %3 != 0){
+                currentPosition++;
+            }
+            start = (currentPosition-1) * getPageInfo.getCountList() + 1;
+            last = (currentPosition) * getPageInfo.getCountList();
+
+
+            for(int i = 0;i<chattingDetail.size(); i++){
+                if(i>=start-1 && i<=last-1){
+                    listItemRes.add(chattingDetail.get(i));
+                }
+            }
+
+            return new BasePageResponse<>(getPageInfo,chattingDetail);
         }catch (BaseException exception){
-            return new BaseResponse<>((exception.getStatus()));
+            return new BasePageResponse<>((exception.getStatus()));
         }
     }
 }
